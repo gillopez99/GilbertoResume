0df17412592fd5f3ea2a29aec1b872bb98fc29b4
gillopez99 <gil.lopez1231a@gmail.com>
2023-06-18 21:33:22 -0700

    Updates

---
+++ precache-manifest.bc612aa2d200cba16b4ebf738408be0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bf8846189ac1be78ca1",
+    "url": "/GilbertoResume/static/css/main.15c8caad.chunk.css"
+  },
+  {
+    "revision": "1bf8846189ac1be78ca1",
+    "url": "/GilbertoResume/static/js/main.0b71a8f9.chunk.js"
+  },
+  {
+    "revision": "fc9cc433b39632e2b6c8",
+    "url": "/GilbertoResume/static/js/runtime~main.036ed222.js"
+  },
+  {
+    "revision": "b721a8ff61a3d78b25d0",
+    "url": "/GilbertoResume/static/css/2.61b1cfbd.chunk.css"
+  },
+  {
+    "revision": "b721a8ff61a3d78b25d0",
+    "url": "/GilbertoResume/static/js/2.1b629681.chunk.js"
+  },
+  {
+    "revision": "f7e540902f59a1d6a93a",
+    "url": "/GilbertoResume/static/js/3.dca0faba.chunk.js"
+  },
+  {
+    "revision": "f6ad0d79086118e86dab98cafa02bb07",
+    "url": "/GilbertoResume/index.html"
+  }
+];
